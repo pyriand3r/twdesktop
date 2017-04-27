17fe62746c4a78da7a5efe4a0e8003286095060f
Gabriel Jülke <pyriand3r@gmail.com>
2017-04-27 14:56:24 +0200

    keep app running even if all windows are closed

--- app/main.js
+++ app/main.js
@@ -4,6 +4,17 @@ const fs = require('fs');
 const WikiWindow = require('./main/WikiWindow.js');
 const config = require('../config');
 
+/**
+ * @listener
+ * Keep app running even if all windows are closed 
+ */
+app.on('window-all-closed', function () {
+    return false;
+});
+
+/**
+ * @listener
+ */
 app.on('ready', function () {
 
     let wiki = new WikiWindow(config.wikiFile);
