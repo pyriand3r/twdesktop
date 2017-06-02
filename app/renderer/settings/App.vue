fa5d2841f7c984728f0f4d61f2d33e5952253159
Gabriel Jülke <pyriand3r@gmail.com>
2017-06-02 21:55:27 +0200

    added basic vue app with bootstrap

--- app/renderer/settings/App.vue
+++ app/renderer/settings/App.vue
@@ -1,3 +1,10 @@
 <template>
-    <div>Settings</div>
+    <div class="container">
+        <b-alert show>
+            Default Alert
+        </b-alert>
+    </div>
 </template>
+
+<script>
+</script>
--- app/renderer/settings/settings.js
+++ app/renderer/settings/settings.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue';
 import app from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
 Vue.component('app', app);
 
 new Vue({
